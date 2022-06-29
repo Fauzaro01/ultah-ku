cd9e05d54a046416b1af6b09ad61eede7c94445c
Fauzaro01 <MuhamadFauzan4750@gmail.com>
2022-06-29 13:44:03 +0700

    Finish :v

---
+++ Procfile
@@ -0,0 +1,1 @@
+web worker: npm start
--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || '3000';
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.render('index')
+});
+
+app.get('*', (req, res) => {
+    res.redirect('/')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
